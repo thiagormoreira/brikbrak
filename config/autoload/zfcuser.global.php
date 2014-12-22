624287df2472bf0b947958b3f9f9db0b484cca77
Thiago Moreira <loganguns@gmail.com>
2014-12-22 18:30:59 -0200

    22/12

--- config/autoload/zfcuser.global.php
+++ config/autoload/zfcuser.global.php
@@ -21,7 +21,7 @@ $settings = array(
      * instead of the default one provided. Default is ZfcUser\Entity\User.
      * The entity class should implement ZfcUser\Entity\UserInterface
      */
-    'user_entity_class' => 'Application\Entity\User',
+    //'user_entity_class' => 'Application\Entity\User',
 
     /**
      * Enable registration
